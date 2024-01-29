fbd3d635637a5cbc61df1c059f2692c902f3e7d3
saturn <157832049+saturnquack@users.noreply.github.com>
2024-01-29 12:40:37 -0500

    Create README.ino

---
+++ README.ino
@@ -0,0 +1,11 @@
+- 👋 Hi, I’m @saturnquack
+- 👀 I’m interested in nothing
+- 🌱 I’m currently learning c++ (arduino/robotics)
+- 💞️ I’m looking to collaborate on absoutly nothing
+- 📫 How to reach me: discord / saturn_quack
+- 😄 Pronouns: she/her
+- ⚡ Fun fact: i like ducks
+
+<!---
+saturnquack is a ✨ catgirl ✨ 
+--->
