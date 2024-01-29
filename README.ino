10f54c3b5a7a12a9205f0b2fcaf4c6a5ce26db2a
saturn <157832049+saturnquack@users.noreply.github.com>
2024-01-29 13:02:47 -0500

    Update README.ino

--- README.ino
+++ README.ino
@@ -1,11 +1,10 @@
-- 👋 Hi, I’m @saturnquack
-- 👀 I’m interested in nothing
-- 🌱 I’m currently learning c++ (arduino/robotics)
-- 💞️ I’m looking to collaborate on absoutly nothing
-- 📫 How to reach me: discord / saturn_quack
-- 😄 Pronouns: she/her
-- ⚡ Fun fact: i like ducks
+- I’m @saturnquack
+- I’m currently learning c++ (arduino/robotics)
+- I’m looking to collaborate on absoutly nothing
+- How to reach me: discord / saturn_quack
+- Pronouns: she/her
+- Fun fact: i like ducks
 
-<!---
-saturnquack is a ✨ catgirl ✨ 
 --->
+  saturnquack is a ✨ catgirl ✨ 
+<---
