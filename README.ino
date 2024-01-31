41f90be9b3d1df44cc5563d2e583787c50f2caf5
saturn <157832049+saturnquack@users.noreply.github.com>
2024-01-31 11:00:52 -0500

    Update README.ino

--- README.ino
+++ README.ino
@@ -2,7 +2,6 @@
 - I’m currently learning c++ (arduino/robotics)
 - I’m looking to collaborate on absoutly nothing
 - How to reach me: discord / saturn_quack
-- Pronouns: she/her
 - Fun fact: i like ducks
 
 --->
