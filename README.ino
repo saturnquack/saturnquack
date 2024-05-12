6531c05fb7ebffc2064c0e9dd381a246a069c393
saturn <157832049+saturnquack@users.noreply.github.com>
2024-05-11 22:01:04 -0400

    Update README.ino

--- README.ino
+++ README.ino
@@ -1,4 +1,20 @@
+void setup() {
 //Hello! I am saturn
-//I’m currently learning c++ (arduino/robotics) using Wokwi.com
-//Reach me on Discord: @saturn_quack
+//Coding in C++ For robotics since August 2023
+}
+
+void catGirl() {
 //government certified ✨ catgirl ✨ 
+}
+
+void socials() {
+//Reach me on Discord: @saturn_quack
+//https://wokwi.com/makers/saturn_quack
+//https://www.tinkercad.com/users/2PbGkN9jdSY
+//linktr.ee/saturn.net
+} 
+
+void loop() {
+ for(;;){
+  delay(1);
+}
